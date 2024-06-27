7288b8001020fcb87371f2f61fdfd3c501d68a30
Bilal Yunus <bilal.yunus@focusteck.com>
2024-06-27 23:05:43 +0500

    phonebook exercises part 2 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Persons from './Persons';
 import Form from './Form';
 import Filter from './Filter';
+import axios from 'axios'
+import './App.css'
 
 const App = () => {
-  const [originalData,setOriginalData] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [originalData,setOriginalData] = useState([]);
   const [persons, setPersons] = useState(originalData);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
+  const [successMsg,setSuccessMsg] = useState(null);
+  const [errorMsg,setErrorMsg] = useState(null);
+
+  useEffect(()=>{
+    getData();
+  },[])
+
+  function getData(){
+    axios.get('http://localhost:3001/persons')
+    .then((res)=>{
+      setOriginalData(res.data);
+      setPersons(res.data);
+    })
+    .catch((error)=>
+      console.error(error)
+    )
+  }
 
   const handleChange=(e)=>{
     setNewName(e.target.value);
@@ -26,27 +40,66 @@ const App = () => {
   const handleChangeNumber=(e)=>{
     setNewNumber(e.target.value);
   }
-  const handleAdd =(e)=>{
+  const handleAdd =async(e)=>{
+    try{
     e.preventDefault();
     const names = persons.map((person)=>person.name)
     if(!names.includes(newName)){
-    setPersons((prev)=>{return [...prev,{name:newName,number:newNumber,id:persons.length+1}]});
-    setOriginalData((prev)=>{return [...prev,{name:newName,number:newNumber,id:persons.length+1}]})
+      await axios.post(`http://localhost:3001/persons`,{name:newName,number:newNumber});
+      getData();
+      setSuccessMsg(`${newName} has been added successfully`);
+      setTimeout(() => {
+        setSuccessMsg(null);
+      }, 2000);
     setNewName('');
+    setNewNumber('');
     }
     else{
-      alert(`${newName} is already added to phonebook`)
+      const existingId = persons.find((name)=>name.name===newName).id;
+      if(window.confirm(`${newName} is already added to phonebook, replace the old phone number with new one?`)){
+        await axios.patch(`http://localhost:3001/persons/${existingId}`,{number:newNumber});
+        getData();
+        setSuccessMsg(`Phone number for ${newName} has been added updated`);
+      setTimeout(() => {
+        setSuccessMsg(null);
+      }, 2000);
+      setNewName('');
+    setNewNumber('');
+      }
+    }
+  }catch(error){
+    console.log(error);
+  }
+  }
+
+  const handleDelete = async(id)=>{
+    try{
+    const newData = await axios.delete(`http://localhost:3001/persons/${id}`);
+    setSuccessMsg(`Record has been removed successfully`);
+      setTimeout(() => {
+        setSuccessMsg(null);
+      }, 2000);
+    getData();
+    }
+    catch(error){
+      console.log(error);
+      setErrorMsg(`Record has been removed already`);
+      setTimeout(() => {
+        setErrorMsg(null);
+      }, 2000);
     }
   }
 
   return (
     <div>
+      {successMsg && <p className='success'>{successMsg}</p>}
+      {errorMsg && <p className='error'>{errorMsg}</p>}
       <h2>Phonebook</h2>
       <Filter filter={filter} handleChangeFilter={handleChangeFilter}/>
       <h2>Add a new</h2>
       <Form newName={newName} newNumber={newNumber} handleAdd={handleAdd} nameChange={handleChange} numberChange={handleChangeNumber}/>
       <h2>Numbers</h2>
-      <Persons persons={persons}/>
+      <Persons persons={persons} handleDelete={handleDelete}/>
     </div>
   )
 }
--- part2/phonebook/src/Persons.jsx
+++ part2/phonebook/src/Persons.jsx
@@ -1,6 +1,14 @@
-const Persons = ({persons}) => {
+const Persons = ({persons,handleDelete}) => {
     return (
-        persons.map((person)=><div key={person.id}>{person.name} {person.number}</div>)
+        persons.map((person)=>
+            <div key={person.id} style={{display:"flex",gap:'5px'}}>
+                <span>
+                    {person.name} {person.number}
+                </span>
+                <button onClick={()=>handleDelete(person.id)}>
+                    delete
+                </button>
+            </div>)
     )
   }
   
